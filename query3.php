267e31a666fedfdf8c61b59ca7f06575e14e009f
Sobhan <72103946+Sobhan-M@users.noreply.github.com>
2021-11-28 19:17:36 -0500

    Current Query 3. Still Needs Work.

--- query3.php
+++ query3.php
@@ -12,7 +12,7 @@
 
 <form method = "post" class="form-input">
 	<label for = "name"> Name * </label>
-	<input type = "text" id = "name" name="name" placeholder="General Hospital" required/>
+	<input type = "text" id = "name" name="name" placeholder="General Hospital"/>
 	<label for = "name"> Street Address </label>
 	<input type = "text" id = "address" name="address" placeholder="Guy Street" />
 	<label for = "name"> City </label>
@@ -51,14 +51,14 @@
 		<option value = "WALKIN-ALLOWED"> Walk-In Allowed </option>
 	</select>
 	<label for = "name"> Capacity * </label>
-	<input type = "text" id = "capacity" name="capacity" placeholder="10" pattern="[0-9]+" required/>
+	<input type = "text" id = "capacity" name="capacity" placeholder="10" pattern="[0-9]+"/>
 	<label for = "name"> Manager ID </label>
 	<input type = "text" id = "managerID" name="managerID" placeholder="9"/>
 	<button type = "submit" name = "insert" value = "insert"> Insert Value </button>
 
 
-	<label for = "name"> Name * </label>
-	<input type = "text" id = "name" name="name" placeholder="General Hospital" required/>
+	<label for = "queryName"> Query Name </label>
+	<input type = "text" id = "queryName" name="queryName" placeholder="General Hospital"/>
 	<button type = "submit" name = "query" value = "query"> Query Table </button>
 	<button type = "submit" name = "delete" value = "delete"> Delete Value </button>
 	<button type = "submit" name = "update" value = "update"> Update Value </button>
@@ -68,21 +68,23 @@
 
 	if ($_POST != null)
 	{
+		// Inserting stuff.
 		if ($_POST["insert"] != NULL)
 		{
 			$name = "'".$_POST["name"]."'";
 			$address = $_POST["address"] == "" ? "DEFAULT" : "'".$_POST["address"]."'";
+			$city = $_POST["city"] == "" ? "DEFAULT" : "'".$_POST["city"]."'";
 			$province = "'".$_POST["province"]."'";
 			$country = $_POST["country"] == "" ? "DEFAULT" : "'".$_POST["country"]."'";
 			$phoneNumber = $_POST["phoneNumber"] == "" ? "DEFAULT" : "'".$_POST["phoneNumber"]."'";
 			$webAddress = $_POST["webAddress"] == "" ? "DEFAULT" : "'".$_POST["webAddress"]."'";
 			$facilityType = "'".$_POST["facilityType"]."'";
 			$category = $_POST["category"] == "" ? "DEFAULT" : "'".$_POST["category"]."'";
-			$capacity = $_POST["capacity"];
+			$capacity = $_POST["capacity"] == "" ? 0 : $_POST["capacity"];
 			$managerID = $_POST["managerID"] == "" ? "DEFAULT" : "'".$_POST["managerID"]."'";
 			$insert = "
-			INSERT INTO PublicHealthFacilities (name, address, province, country, phoneNumber, webAddress, facilityType, category, capacity, managerID)
-			VALUES ($name, $address, $province, $country, $phoneNumber, $webAddress, $facilityType, $category, $capacity, $managerID);";
+			INSERT INTO PublicHealthFacilities (name, address, city, province, country, phoneNumber, webAddress, facilityType, category, capacity, managerID)
+			VALUES ($name, $address, $city, $province, $country, $phoneNumber, $webAddress, $facilityType, $category, $capacity, $managerID);";
 
 			if ($conn->query($insert) === TRUE) {
 				echo "<p> Successfully entered the result!</p>";
@@ -91,73 +93,127 @@
 			}
 		}
 
+		// Querying stuff.
 		if ($_POST["query"] != NULL)
 		{
-			$name = "'".$_POST["name"]."'";
-			$address = $_POST["address"] == "" ? "DEFAULT" : "'".$_POST["address"]."'";
-			$province = "'".$_POST["province"]."'";
-			$country = $_POST["country"] == "" ? "DEFAULT" : "'".$_POST["country"]."'";
-			$phoneNumber = $_POST["phoneNumber"] == "" ? "DEFAULT" : "'".$_POST["phoneNumber"]."'";
-			$webAddress = $_POST["webAddress"] == "" ? "DEFAULT" : "'".$_POST["webAddress"]."'";
-			$facilityType = "'".$_POST["facilityType"]."'";
-			$category = $_POST["category"] == "" ? "DEFAULT" : "'".$_POST["category"]."'";
-			$capacity = $_POST["capacity"];
-			$managerID = $_POST["managerID"] == "" ? "DEFAULT" : "'".$_POST["managerID"]."'";
-			$insert = "
-			INSERT INTO PublicHealthFacilities (name, address, province, country, phoneNumber, webAddress, facilityType, category, capacity, managerID)
-			VALUES ($name, $address, $province, $country, $phoneNumber, $webAddress, $facilityType, $category, $capacity, $managerID);";
-
-			if ($conn->query($insert) === TRUE) {
-				echo "<p> Successfully entered the result!</p>";
-			} else {
-				echo "<p> Error: " . $insert . ": <br>" . $conn->error . "</p>";
+			if ($_POST["queryName"] != "")
+			{
+				$name = "'".$_POST["queryName"]."'";
+				$query = "
+				SELECT *
+				FROM PublicHealthFacilities
+				WHERE name = $name";
 			}
+			else
+			{
+				$query = "
+				SELECT *
+				FROM PublicHealthFacilities";
+			}
+
+			$result = $conn->query($query);
+
+			if (mysqli_num_rows($result) > 0) 
+			{
+				echo "<h2> Healthcare Facilities </h2>";
+				echo "
+					<table>
+						<tr>
+							<th> Facility Name </th>
+							<th> Address </th>
+							<th> City </th>
+							<th> Province </th>
+							<th> Country </th>
+							<th> Phone Number </th>
+							<th> Web Address </th>
+							<th> Facility Type </th>
+							<th> Category </th>
+							<th> Capacity </th>
+							<th> Manager ID </th>
+						</tr>";
+
+				while($row = mysqli_fetch_assoc($result)) 
+				{
+					echo "<tr>
+							<td> ".$row["name"]." </td>
+							<td> ".$row["address"]." </td>
+							<td> ".$row["city"]." </td>
+							<td> ".$row["province"]." </td>
+							<td> ".$row["country"]." </td>
+							<td> ".$row["phoneNumber"]." </td>
+							<td> ".$row["webAddress"]." </td>
+							<td> ".$row["facilityType"]." </td>
+							<td> ".$row["category"]." </td>
+							<td> ".$row["capacity"]." </td>			
+							<td> ".$row["managerID"]." </td>					
+						</tr>";
+				}
+				echo "</table>";
+			}
+			else
+			{
+				echo "<p>".$city." has no healthcare facilities! </p>";
+			}
+				
 		}
 
 		if ($_POST["delete"] != NULL)
 		{
-			$name = "'".$_POST["name"]."'";
-			$address = $_POST["address"] == "" ? "DEFAULT" : "'".$_POST["address"]."'";
-			$province = "'".$_POST["province"]."'";
-			$country = $_POST["country"] == "" ? "DEFAULT" : "'".$_POST["country"]."'";
-			$phoneNumber = $_POST["phoneNumber"] == "" ? "DEFAULT" : "'".$_POST["phoneNumber"]."'";
-			$webAddress = $_POST["webAddress"] == "" ? "DEFAULT" : "'".$_POST["webAddress"]."'";
-			$facilityType = "'".$_POST["facilityType"]."'";
-			$category = $_POST["category"] == "" ? "DEFAULT" : "'".$_POST["category"]."'";
-			$capacity = $_POST["capacity"];
-			$managerID = $_POST["managerID"] == "" ? "DEFAULT" : "'".$_POST["managerID"]."'";
-			$insert = "
-			INSERT INTO PublicHealthFacilities (name, address, province, country, phoneNumber, webAddress, facilityType, category, capacity, managerID)
-			VALUES ($name, $address, $province, $country, $phoneNumber, $webAddress, $facilityType, $category, $capacity, $managerID);";
+			if ($_POST["queryName"] != null)
+			{
+				$name = "'".$_POST["queryName"]."'";
+				$query = "
+				DELETE FROM PublicHealthFacilities
+				WHERE name = $name";
 
-			if ($conn->query($insert) === TRUE) {
-				echo "<p> Successfully entered the result!</p>";
-			} else {
-				echo "<p> Error: " . $insert . ": <br>" . $conn->error . "</p>";
+				if ($conn->query($query) === TRUE) {
+					echo "<p> Successfully deleted the entry!</p>";
+				} else {
+					echo "<p> Error: " . $insert . ": <br>" . $conn->error . "</p>";
+				}
 			}
+			else
+			{
+				echo "<p> Please fill the 'Query Name' input with the facility you want to delete. </p>";
+			}
+			
 		}
 
+		// Updating stuff.
 		if ($_POST["update"] != NULL)
 		{
-			$name = "'".$_POST["name"]."'";
-			$address = $_POST["address"] == "" ? "DEFAULT" : "'".$_POST["address"]."'";
-			$province = "'".$_POST["province"]."'";
-			$country = $_POST["country"] == "" ? "DEFAULT" : "'".$_POST["country"]."'";
-			$phoneNumber = $_POST["phoneNumber"] == "" ? "DEFAULT" : "'".$_POST["phoneNumber"]."'";
-			$webAddress = $_POST["webAddress"] == "" ? "DEFAULT" : "'".$_POST["webAddress"]."'";
-			$facilityType = "'".$_POST["facilityType"]."'";
-			$category = $_POST["category"] == "" ? "DEFAULT" : "'".$_POST["category"]."'";
-			$capacity = $_POST["capacity"];
-			$managerID = $_POST["managerID"] == "" ? "DEFAULT" : "'".$_POST["managerID"]."'";
-			$insert = "
-			INSERT INTO PublicHealthFacilities (name, address, province, country, phoneNumber, webAddress, facilityType, category, capacity, managerID)
-			VALUES ($name, $address, $province, $country, $phoneNumber, $webAddress, $facilityType, $category, $capacity, $managerID);";
+			if ($_POST["queryName"] != null)
+			{
+				$name = "'".$_POST["name"]."'";
+				$address = $_POST["address"] == "" ? "DEFAULT" : "'".$_POST["address"]."'";
+				$province = "'".$_POST["province"]."'";
+				$country = $_POST["country"] == "" ? "DEFAULT" : "'".$_POST["country"]."'";
+				$phoneNumber = $_POST["phoneNumber"] == "" ? "DEFAULT" : "'".$_POST["phoneNumber"]."'";
+				$webAddress = $_POST["webAddress"] == "" ? "DEFAULT" : "'".$_POST["webAddress"]."'";
+				$facilityType = "'".$_POST["facilityType"]."'";
+				$category = $_POST["category"] == "" ? "DEFAULT" : "'".$_POST["category"]."'";
+				$capacity = $_POST["capacity"];
+				$managerID = $_POST["managerID"] == "" ? "DEFAULT" : "'".$_POST["managerID"]."'";
 
-			if ($conn->query($insert) === TRUE) {
-				echo "<p> Successfully entered the result!</p>";
-			} else {
-				echo "<p> Error: " . $insert . ": <br>" . $conn->error . "</p>";
+				$queryName = "'".$_POST["queryName"]."'";
+				$insert = "
+				UPDATE PublicHealthFacilities
+				SET name = $name, address = $address, province = $province, country = $country,
+					phoneNumber = $phoneNumber, webAddress = $webAddress, facilityType = $facilityType, 
+					category = $category, capacity = $capacity, managerID = $managerID
+				WHERE name = $queryName";
+
+				if ($conn->query($insert) === TRUE) {
+					echo "<p> Successfully updated the entry!</p>";
+				} else {
+					echo "<p> Error: " . $insert . ": <br>" . $conn->error . "</p>";
+				}
 			}
+			else
+			{
+				echo "<p> Please fill the 'Query Name' input with the facility you want to update. </p>";
+			}
+			
 		}
 		
 	}
